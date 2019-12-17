5b6e047b80bbc249cda98233ba88ca8b913c34b1
League Student <league@iMac-9.attlocal.net>
2019-12-16 16:54:45 -0800

    CrazyDigitalPainting, PixelArt, started ConwaysGameOfLife

--- src/_03_Conways_Game_of_Life/Cell.java
+++ src/_03_Conways_Game_of_Life/Cell.java
@@ -28,7 +28,15 @@ public class Cell implements Drawable{
 	 * (source: Wikipedia)
 	 * */
 	public void liveOrDie(int numNeighbors) {
-		
+		if(numNeighbors<2) {
+			isAlive=false;
+		}else if(numNeighbors==2) {
+			isAlive=true;
+		}else if(numNeighbors==3) {
+			isAlive=true;
+		}else if(numNeighbors>3) {
+			isAlive=false;
+		}
 	}
 	
 	public int getX() {
